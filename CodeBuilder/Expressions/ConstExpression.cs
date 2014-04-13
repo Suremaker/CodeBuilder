c71bb4f297f58c2e2a2d50bdd2d4041483088ae4
Suremaker <wojciech.kotlarski@gmail.com>
2014-04-13 21:30:37 +0100

    added const expressions for long, float and double

--- CodeBuilder/Expressions/ConstExpression.cs
+++ CodeBuilder/Expressions/ConstExpression.cs
@@ -7,38 +7,84 @@ namespace CodeBuilder.Expressions
     {
         private delegate void CompileFn(IBuildContext ctx);
         private readonly object _value;
-        private readonly OpCode _opCode;
         private readonly CompileFn _action;
 
         public ConstExpression(int value)
             : base(typeof(int))
         {
             _value = value;
-            _opCode = FindIntOpCode(value);
             _action = CompileInt;
+        }
+
+        public ConstExpression(long value)
+            : base(typeof(long))
+        {
+            _value = value;
+            _action = CompileLong;
+        }
+
+        public ConstExpression(float value)
+            : base(typeof(float))
+        {
+            _value = value;
+            _action = CompileFloat;
+        }
+
+        public ConstExpression(double value)
+            : base(typeof(double))
+        {
+            _value = value;
+            _action = CompileDouble;
         }
 
         public ConstExpression(string value)
             : base(typeof(string))
         {
             _value = value;
-            _opCode = OpCodes.Ldstr;
             _action = CompileStr;
         }
 
         private void CompileStr(IBuildContext ctx)
         {
-            ctx.Generator.Emit(_opCode, (string)_value);
+            ctx.Generator.Emit(OpCodes.Ldstr, (string)_value);
         }
 
         private void CompileInt(IBuildContext ctx)
         {
-            if (_opCode == OpCodes.Ldc_I4_S)
-                ctx.Generator.Emit(_opCode, (sbyte)(int)_value);
-            else if(_opCode == OpCodes.Ldc_I4)
-                ctx.Generator.Emit(_opCode, (int)_value);
+            var opCode = FindIntOpCode((int)_value);
+            EmitIntCode(ctx, opCode);
+        }
+
+        private void CompileLong(IBuildContext ctx)
+        {
+            var value = (long)_value;
+            if (value >= int.MinValue && value <= int.MaxValue)
+            {
+                EmitIntCode(ctx, FindIntOpCode((int)value));
+                ctx.Generator.Emit(OpCodes.Conv_I8);
+            }
             else
-                ctx.Generator.Emit(_opCode);
+                ctx.Generator.Emit(OpCodes.Ldc_I8, value);
+        }
+
+        private void CompileFloat(IBuildContext ctx)
+        {
+            ctx.Generator.Emit(OpCodes.Ldc_R4, (float)_value);
+        }
+
+        private void CompileDouble(IBuildContext ctx)
+        {
+            ctx.Generator.Emit(OpCodes.Ldc_R8, (double)_value);
+        }
+
+        private void EmitIntCode(IBuildContext ctx, OpCode opCode)
+        {
+            if (opCode == OpCodes.Ldc_I4_S)
+                ctx.Generator.Emit(opCode, (sbyte)(int)_value);
+            else if (opCode == OpCodes.Ldc_I4)
+                ctx.Generator.Emit(opCode, (int)_value);
+            else
+                ctx.Generator.Emit(opCode);
         }
 
         private OpCode FindIntOpCode(int value)
@@ -53,7 +99,7 @@ namespace CodeBuilder.Expressions
             if (value == 7) return OpCodes.Ldc_I4_7;
             if (value == 8) return OpCodes.Ldc_I4_8;
             if (value == -1) return OpCodes.Ldc_I4_M1;
-            if (value >= -128 && value <= 127) return OpCodes.Ldc_I4_S;
+            if (value >= sbyte.MinValue && value <= sbyte.MaxValue) return OpCodes.Ldc_I4_S;
             return OpCodes.Ldc_I4;
         }
 
